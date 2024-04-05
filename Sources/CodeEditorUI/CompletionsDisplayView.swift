a20109aebebe21b4e68e5866e137badd5252ace6
Miguel de Icaza <miguel@gnome.org>
2024-04-05 10:45:03 -0400

    Plenty of styling, shadow, higher intensity palette, move to LazyGrid rather than Grid

--- Sources/CodeEditorUI/CompletionsDisplayView.swift
+++ Sources/CodeEditorUI/CompletionsDisplayView.swift
@@ -8,29 +8,30 @@
 import SwiftUI
 
 public struct CompletionsDisplayView: View {
+    @Environment(\.colorScheme) var colorScheme
     let prefix: String
     var completions: [CompletionEntry]
     @State var selected = 0
     
-    func getDefaultAcceptButton () -> some View {
+    func getDefaultAcceptButton (_ color: Color) -> some View {
         Image (systemName: "return")
             .padding(5)
-            .background { Color.accentColor }
-            .foregroundStyle(.background)
+            .background { color }
+            .foregroundStyle(Color.secondary)
             .clipShape(RoundedRectangle(cornerRadius: 4))
     }
     
     let palette: [Color] = [
-        Color (#colorLiteral(red: 1, green: 0.7014271617, blue: 0.5018826723, alpha: 1)),
-        Color (#colorLiteral(red: 1, green: 0.8414883018, blue: 0.5013846755, alpha: 1)),
-        Color (#colorLiteral(red: 0.8560311794, green: 0.7036848664, blue: 0.9709442258, alpha: 1)),
-        Color (#colorLiteral(red: 0.5355303288, green: 0.6661332846, blue: 0.8973312974, alpha: 1)),
-        Color (#colorLiteral(red: 0.6944738626, green: 0.6490275264, blue: 0.8922163844, alpha: 1)),
-        Color (#colorLiteral(red: 0.7470750213, green: 0.9311752915, blue: 0.5108862519, alpha: 1)),
-        Color (#colorLiteral(red: 0.5022051334, green: 0.8237985969, blue: 0.759318471, alpha: 1)),
-        Color (#colorLiteral(red: 0.5644295812, green: 0.866447866, blue: 0.973231256, alpha: 1)),
-        Color (#colorLiteral(red: 0.9982370734, green: 0.721807301, blue: 0.8741931319, alpha: 1)),
-        Color (#colorLiteral(red: 0.8962232471, green: 0.5777660608, blue: 0.7532460093, alpha: 1)),
+        Color (#colorLiteral(red: 1, green: 0.5204778927, blue: 0.2, alpha: 1)),
+        Color (#colorLiteral(red: 1, green: 0.7472373315, blue: 0.2049082724, alpha: 1)),
+        Color (#colorLiteral(red: 0.6337333333, green: 0.194, blue: 0.97, alpha: 1)),
+        Color (#colorLiteral(red: 0.18, green: 0.4399056839, blue: 0.9, alpha: 1)),
+        Color (#colorLiteral(red: 0.3110562249, green: 0.178, blue: 0.89, alpha: 1)),
+        Color (#colorLiteral(red: 0.6041037997, green: 0.93, blue: 0.186, alpha: 1)),
+        Color (#colorLiteral(red: 0.164, green: 0.82, blue: 0.6884707017, alpha: 1)),
+        Color (#colorLiteral(red: 0.194, green: 0.7673004619, blue: 0.97, alpha: 1)),
+        Color (#colorLiteral(red: 1, green: 0.2, blue: 0.6410113414, alpha: 1)),
+        Color (#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)),
         ]
 
     func kindToImage (kind: CompletionEntry.CompletionKind) -> some View {
@@ -39,43 +40,44 @@ public struct CompletionsDisplayView: View {
         
         switch kind {
         case .class:
-            image = "cube"
+            image = "c.square.fill"
             color = palette[0]
         case .function:
-            image = "function"
+            image = "f.square.fill"
             color = palette[1]
         case .constant:
-            image = "c.square"
+            image = "c.square.fill"
             color = palette[2]
         case .enum:
-            image = "e.square"
+            image = "e.square.fill"
             color = palette[3]
         case .filePath:
-            image = "folder"
+            image = "folder.circle.fill"
             color = palette[4]
         case .member:
-            image = "m.square"
+            image = "m.square.fill"
             color = palette[5]
         case .nodePath:
-            image = "point.3.connected.trianglepath.dotted"
+            image = "n.square.fill"
             color = palette[6]
         case .plainText:
-            image = "text.justify.left"
+            image = "t.square.fill"
             color = palette[7]
         case .signal:
-            image = "app.connected.to.app.below.fill"
+            image = "s.square.fill"
             color = palette[8]
         case .variable:
-            image = "shippingbox"
+            image = "v.square.fill"
             color = palette[9]
         }
         return Image (systemName: image)
             .resizable()
             .scaledToFit()
-            .padding (4)
-            .background { color }
-            .padding ([.trailing], 5)
-            .fontWeight(.light)
+            .padding(1)
+            .foregroundStyle(Color.white, color)
+            .fontWeight(.regular)
+            .frame(height: 20)
+            //.frame(width: 20, height: 40)
     }
     
     /// Makes bold text for the text that we were matching against
@@ -84,15 +86,17 @@ public struct CompletionsDisplayView: View {
         var ra = AttributedString()
         let sourceLower = source.lowercased()
         var scan = sourceLower [sourceLower.startIndex...]
+        let plain = UIColor.label
+        let bolded = UIColor.label.withAlphaComponent(0.6)
         for hs in hayStack {
             let match = hs.lowercased().first ?? hs
             
             var ch = AttributedString ("\(hs)")
             if scan.count > 0, let p = scan.firstIndex(of: match) {
-                ch.foregroundColor = .primary
+                ch.foregroundColor = plain
                 scan = scan [scan.index(after: p)...]
             } else {
-                ch.foregroundColor = .secondary
+                ch.foregroundColor = bolded
             }
             ra.append (ch)
         }
@@ -102,46 +106,53 @@ public struct CompletionsDisplayView: View {
     func item (prefix: String, _ v: CompletionEntry) -> some View {
         HStack (spacing: 0){
             boldify (prefix, v.display)
+            Spacer()
         }
         .padding (3)
         .padding ([.horizontal], 3)
     }
 
     public var body: some View {
+        let highlight = colorScheme == .dark ? Color.red : Color(red: 0.8, green: 0.87, blue: 0.96)
+
         ScrollView(.vertical){
-            Grid (alignment: .leading, verticalSpacing: 4) {
-                ForEach (Array(completions.enumerated()), id: \.offset) { idx, entry in
-                    GridRow (alignment: .firstTextBaseline) {
-                        kindToImage(kind: entry.kind)
-                        
-                        item (prefix: prefix, entry)
-                            .background {
-                                if idx == selected {
-                                    Color.blue.opacity(0.3)
-                                }
+            LazyVGrid(columns: [
+                GridItem(.flexible(), alignment: .leading),
+                GridItem(.fixed(30), spacing: 3)]){
+                    ForEach (Array(completions.enumerated()), id: \.offset) { idx, entry in
+
+                        HStack {
+                            kindToImage(kind: entry.kind)
+                            item (prefix: prefix, entry)
+                        }
+                        .padding ([.leading], 7)
+                        .background {
+                            if idx == selected {
+                                highlight
                             }
-                            .clipShape(RoundedRectangle(cornerRadius: 4))
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
                         if idx == selected {
-                            getDefaultAcceptButton()
+                            getDefaultAcceptButton(highlight)
                         } else {
-                            EmptyView()
+                            Text("")
                         }
                     }
-                    .frame(height: UIFont.preferredFont(forTextStyle: .body).capHeight*2)
                 }
-            }
         }
-        .frame(minWidth: 200, maxWidth: 300, maxHeight: 34*6)
-        .padding([.leading], 4)
+        .frame(minWidth: 200, maxWidth: 350, maxHeight: 34*6)
+        .padding(4)
         .fontDesign(.monospaced)
         .font(.footnote)
+        .background { Color (uiColor: .systemGray6) }
         .clipShape (RoundedRectangle(cornerRadius: 6, style: .circular))
+        .shadow(color: Color (uiColor: .systemGray5), radius: 3, x: 3, y: 3)
         .overlay {
             RoundedRectangle(cornerRadius: 6, style: .circular)
-                .stroke(Color (uiColor: .systemGroupedBackground), lineWidth: 1) // Add a border
+                .stroke(Color (uiColor: .systemGray3), lineWidth: 1) // Add a border
 
-                //.shadow(color: Color.gray, radius: 3, x: 3, y: 3)
         }
+
     }
 }
 
@@ -169,7 +180,14 @@ struct DemoCompletionsDisplayView: View {
         ]
     }
     var body: some View {
-        CompletionsDisplayView(prefix: "print", completions: completions)
+        HStack {
+            VStack {
+                CompletionsDisplayView(prefix: "print", completions: completions)
+                Spacer ()
+            }
+            Spacer ()
+        }
+        .padding()
     }
 }
 
